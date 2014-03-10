3f8cb1744a6663da73b83cd0136b809d3a9d5a34
Riley Starrs <tstarrs@mail.umw.edu>
2014-03-10 18:28:25 +0000

    updated server.py

--- server.py
+++ server.py
@@ -1,4 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for
-app = Flask(__name__)
+from flask import Flask, render_template, request
 import util
 import MySQLdb
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def mainIndex():
+    return render_template('index.html', selectedMenu = 'Home')
+  
+@app.route('/newDebt')
+def newDebtIndex():
+    return render_template('addADebt.html', selectedMenu = 'NewDebt')
+
+@app.route('/FriendsInDebt')
+def friendDebtIndex():
+    return render_template('FriendDebt.html', selectedMenu = 'FriendsInDebt')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=3000)
