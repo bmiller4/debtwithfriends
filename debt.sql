3034c3ec83a379836fa76a2c486b159310c9cab5
Brennan Miller <bmiller4@mail.umw.edu>
2014-03-10 20:10:18 +0000

    primary key changes

--- debt.sql
+++ debt.sql
@@ -5,29 +5,32 @@ GRANT ALL PRIVILEGES ON debtDB.* to 'debt'@'localhost'
 identified by 'password';
 USE debtDB;
 
-CREATE TABLE friend_list (
-  id /*Auto increment here? also primary key?*/
+CREATE TABLE main_list (
+  id INT NOT NULL AUTO_INCREMENT,
   firstname VARCHAR(25),
   lastname VARCHAR(25),
   total_debt INT(1000),
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE friend_debt (
-  id /*primary key*/  
+  id INT NOT NULL AUTO_INCREMENT,
   transaction VARCHAR(50),
   debt_amount INT(100),
-  description VARCHAR(100)
+  description VARCHAR(100),
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE user_debt (
-  id /*primary key*/
+  id INT NOT NULL AUTO_INCREMENT,
   transaction VARCHAR(50),
   debt_amount INT(100),
   description VARCHAR(100)
+  PRIMARY KEY(id)
 );
  
 
-INSERT INTO friend_list VALUES ( /*id*/, 'Brennan','Miller', 10);
-INSERT INTO friend_list VALUES ( /*id*/, 'Austin','Bouchard', 30);
-INSERT INTO friend_list VALUES ( /*id*/, 'Riley','Starrs', 50);
-INSERT INTO friend_list VALUES ( /*id*/, 'Patrick','Stalcup', -59);
+INSERT INTO main_list VALUES ('Brennan','Miller', 10);
+INSERT INTO main_list VALUES ('Austin','Bouchard', 30);
+INSERT INTO main_list VALUES ('Riley','Starrs', 50);
+INSERT INTO main_list VALUES ('Patrick','Stalcup', -59);
