03efa6f50463684ff76d71b24184924ec9d0de22
Brennan Miller <bmiller4@mail.umw.edu>
2014-03-11 03:31:45 +0000

    Fixed friend_debt display

--- server.py
+++ server.py
@@ -17,29 +17,30 @@ def homePage():
   return render_template('index.html', main_list=rows, selectedMenu = 'Home')
 
 @app.route('/addaDebt')
-def addDebt():
-    #query = "INSERT INTO friend_debt (friend_lastname, debt_amount, description) VALUES ('";
-    #query += request.form['friend_lastname'] + "', '" + request.form['debt_amount'] + "', '" + request.form['description'] + "')"
-    #query = 'UPDATE main_list SET total_debt = total_debt' + form.request(debt_amount) + 'WHERE lastname = ' + form.request(friend_lastname)
-    db = utils.db_connect()
-    cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-    #cur.execute(query)
-    db.commit()
+def addDebtIndex():
     return render_template('addaDebt.html', selectedMenu = 'NewDebt')
   
-@app.route('/addADebt2')
+@app.route('/addaDebt2', methods=['POST'])
 def newDebtIndex():
-    return render_template('addADebt2.html', selectedMenu = 'NewDebt')
+    query = "INSERT INTO friend_debt (friend_lastname, debt_amount, description) VALUES ('";
+    query += request.form['friend_lastname'] + "', '" + request.form['debt_amount'] + "', '" + request.form['description'] + "')"
+    #query2 = "UPDATE main_list SET total_debt = total_debt + " + request.form['debt_amount'] + " WHERE lastname = " + request.form['friend_lastname']
+    db = utils.db_connect()
+    cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+    cur.execute(query)
+    #cur.execute(query2)
+    db.commit()
+    return render_template('addaDebt2.html', selectedMenu = 'NewDebt')
 
 @app.route('/friendDebt')
 def friendDebtIndex():
     db = utils.db_connect()
     cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-    query = 'SELECT * FROM friend_debt'
-    cur.execute(query)
+    query1 = 'SELECT * FROM friend_debt'
+    cur.execute(query1)
     rows = cur.fetchall()
     db.commit()
-    return render_template('friendDebt.html', selectedMenu = 'FriendsInDebt')
+    return render_template('friendDebt.html', selectedMenu = 'FriendsInDebt',friend_debt=rows)
 
 
 if __name__ == '__main__':
