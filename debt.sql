283df0e1f5b93c212122399b4c3777293c792f6e
Riley Starrs <tstarrs@mail.umw.edu>
2014-03-24 21:57:13 +0000

    new tables

--- debt.sql
+++ debt.sql
@@ -1,37 +1,50 @@
 DROP DATABASE debtDB;
 
 CREATE DATABASE IF NOT EXISTS debtDB;
-GRANT ALL PRIVILEGES ON debtDB.* to 'debt'@'localhost'
+GRANT ALL PRIVILEGES ON debtDB.* to 'debt'@'localhost' 
 identified by 'password';
 USE debtDB;
 
-CREATE TABLE main_list (
-  id INT NOT NULL AUTO_INCREMENT,
-  firstname VARCHAR(25),
-  lastname VARCHAR(25),
-  total_debt INT(100),
-  PRIMARY KEY(id)
+CREATE TABLE user_list (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,/*Auto increment here? also primary key?*/
+  name VARCHAR(50),
+  total debt INT(1000)
+);
+CREATE TABLE friend_info (
+  user_id INT NOT NULL, 
+  CONSTRAINT user_list_user_id_fk
+  FOREIGN KEY (user_id)
+  REFERENCES friend_list (user_id)
+  debt_id INT NOT NULL,
+  CONSTRAINT friend_debt_debt_id_fk
+  FOREIGN KEY (debt_id)
+  REFERENCES friend_debt (debt_id)
 );
 
 CREATE TABLE friend_debt (
-  friend_lastname VARCHAR(50),
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY /*primary key*/  
+  transaction VARCHAR(50),
   debt_amount INT(100),
   description VARCHAR(100)
 );
 
-CREATE TABLE user_debt (
-  user_id VARCHAR(50),
+/*CREATE TABLE user_debt (
+  user_id INT NOT NULL,
+  CONSTRAINT friend_list_user_id_fk
+  FOREIGN KEY (user_id)
+  REFERENCES friend_list (user_id)
+  transaction VARCHAR(50),
   debt_amount INT(100),
   description VARCHAR(100)
-);
+);*/
  
 
-INSERT INTO main_list (id, firstname, lastname, total_debt) VALUES (NULL,'Brennan','Miller', 0);
-INSERT INTO main_list (id, firstname, lastname, total_debt) VALUES (NULL,'Austin','Bouchard', -1);
-INSERT INTO main_list (id, firstname, lastname, total_debt) VALUES (NULL,'Riley','Starrs', 0);
-INSERT INTO main_list (id, firstname, lastname, total_debt) VALUES (NULL,'Patrick','Stalcup', 59);
+INSERT INTO user_list VALUES ( /*id*/, 'BrennanMiller', 10);
+INSERT INTO user_list VALUES ( /*id*/, 'Austin Bouchard', 30);
+INSERT INTO user_list VALUES ( /*id*/, 'Riley Starrs', 50);
+INSERT INTO user_list VALUES ( /*id*/, 'Patrick Stalcup', -59);
 
-INSERT INTO friend_debt VALUES('Zacharski', 55, 'stole my girlfriend');
+/*INSERT INTO friend_debt VALUES('Zacharski', 55, 'stole my girlfriend');
 INSERT INTO friend_debt VALUES('Benshoff', 10, 'Back massage');
 INSERT INTO friend_debt VALUES('Patrick', -10, 'let me eat girlscout cookies');
-INSERT INTO user_debt VALUES ('Raz', 1, 'Bummed a smoke');
+INSERT INTO user_debt VALUES ('Raz', 1, 'Bummed a smoke');*/
