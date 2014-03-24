fd483968f607f35ca460e3807aa0a72113f868bd
Riley Starrs <tstarrs@mail.umw.edu>
2014-03-24 22:35:48 +0000

    working sql file, and updated layout

--- debt.sql
+++ debt.sql
@@ -8,26 +8,28 @@ USE debtDB;
 CREATE TABLE user_list (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,/*Auto increment here? also primary key?*/
   username VARCHAR(50),
-  password VARCHAR(20),
-  total debt INT(1000)
-);
-CREATE TABLE friend_info (
-  user_id INT NOT NULL, 
-  CONSTRAINT user_list_user_id_fk
-  FOREIGN KEY (user_id)
-  REFERENCES friend_list (user_id)
-  debt_id INT NOT NULL,
-  CONSTRAINT friend_debt_debt_id_fk
-  FOREIGN KEY (debt_id)
-  REFERENCES friend_debt (debt_id)
+  passwrd VARCHAR(20),
+  total_debt INT(100)
 );
 
 CREATE TABLE friend_debt (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY /*primary key*/  
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, /*primary key*/  
   transaction VARCHAR(50),
   debt_amount INT(100),
   description VARCHAR(100)
 );
+
+CREATE TABLE friend_info (
+  user_id INT NOT NULL, 
+  CONSTRAINT user_list_user_id_fk
+  FOREIGN KEY (user_id)
+  REFERENCES user_list (id),
+  debt_id INT NOT NULL,
+  CONSTRAINT friend_debt_debt_id_fk
+  FOREIGN KEY (debt_id)
+  REFERENCES friend_debt (id)
+);
+
 
 /*CREATE TABLE user_debt (
   user_id INT NOT NULL,
@@ -40,10 +42,9 @@ CREATE TABLE friend_debt (
 );*/
  
 
-INSERT INTO user_list VALUES ( /*id*/, 'BrennanMiller', 10);
-INSERT INTO user_list VALUES ( /*id*/, 'Austin Bouchard', 30);
-INSERT INTO user_list VALUES ( /*id*/, 'Riley Starrs', 50);
-INSERT INTO user_list VALUES ( /*id*/, 'Patrick Stalcup', -59);
+INSERT INTO user_list VALUES (id,'Brennan Miller','peanut', 10);
+INSERT INTO user_list VALUES ( id,'Austin Bouchard','12345', 30);
+INSERT INTO user_list VALUES (id,'Riley Starrs','Riotpls01', 50);
 
 /*INSERT INTO friend_debt VALUES('Zacharski', 55, 'stole my girlfriend');
 INSERT INTO friend_debt VALUES('Benshoff', 10, 'Back massage');
